b60ddaace35b6c66ff9d9dcb77fda753dda1b3f8
sriramarji <52248143+sriramarji@users.noreply.github.com>
2025-07-03 01:15:45 +0530

    modified files

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-module "s3_bucket" {
+module "s3" {
   source      = "./modules/s3"
   bucket_name = var.bucket_name
   tags        = var.tags
@@ -19,3 +19,29 @@ module "iam" {
   source      = "./modules/iam"
   bucket_name = module.s3_bucket.bucket_name
 }
+
+
+module "lambda" {
+  source          = "./modules/lambda"
+  function_name   = var.ingest_lambda_name
+  lambda_zip      = data.archive_file.lambda_zip_ingest.output_path
+  runtime         = var.lambda_runtime
+  role_arn        = module.lambda_iam_role.iam_role_arn
+  environment_vars = {
+    DB_SECRET_NAME = var.db_secret_name
+    RAW_BUCKET     = var.bucket_name
+  }
+  vpc_config = {
+    subnet_ids         = module.vpc.private_subnets
+    security_group_ids = [module.rds_sg.rds_security_group_aurora_id]
+  }
+  tags = var.tags
+
+  depends_on = [
+    module.s3,
+    module.vpc,
+    module.iam,
+    #module.db_secret
+  ]
+}
+
--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -10,7 +10,6 @@ output "private_subnets" {
   value = aws_subnet.private[*].id
 }
 
-
 output "rds_security_group_id" {
   description = "Security group ID for the RDS"
   value       = aws_security_group.my_rds_sg.id 
